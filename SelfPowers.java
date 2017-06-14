4446d2b3ff1e4a3e97ee9b587668676b7ef59aaa
billlee929 <bill.lee929@gmail.com>
2017-06-14 12:34:08 -0400

    self power solution

---
+++ SelfPowers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.math.BigInteger;
+public class SelfPowers{
+	public static void main(String[] args){
+		BigInteger sum = new BigInteger("0");
+		BigInteger mod = new BigInteger("100000000000000000000000");
+		for(int i = 1; i <= 1000; i++){
+			BigInteger x = BigInteger.valueOf(i);
+			BigInteger num = x.modPow(x, mod);
+			sum = sum.add(num);
+		}
+	System.out.println("Here we go " + sum); 
+	}
+
+}
