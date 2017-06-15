66579b032bba968bf3a7c2ca51f271033270bc12
billlee929 <bill.lee929@gmail.com>
2017-06-14 20:41:56 -0400

    Power Digit Sum

---
+++ PowerDigitSum.java
@@ -0,0 +1,21 @@
+import java.math.BigInteger;
+public class PowerDigitSum{
+	
+	public static void main(String[] args){
+		BigInteger large = new BigInteger("2");
+		large = large.pow(1000);
+		String x = large.toString();
+		char a = '0';
+		int sum = 0;
+
+		for(int i = 0; i < x.length(); i++){
+			a = x.charAt(i);
+			int y = Character.getNumericValue(a);
+			sum += y;
+		}
+
+
+		System.out.println("wow, this is " +large);
+		System.out.println("the sum of the digits is " + sum);
+	}
+}
